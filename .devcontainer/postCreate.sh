9c2fbc7ea39c7752ca62beb7cf7cb720b63264c4
abisatyaw <abisatya90@gmail.com>
2025-08-31 12:11:04 +0000

    refactor: postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Ensure .air directory exists
 mkdir -p /workspace/.air
-# Run air in background
 echo "Start development..."
-# Open a shell so VS Code terminal stays active
-exec "$SHELL"
